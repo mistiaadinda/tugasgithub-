29e423340d590d564247c47661ac367705996859
Ronanda1922 <84946806+Ronanda1922@users.noreply.github.com>
2021-05-28 17:07:02 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -6,12 +6,14 @@ $aksi=$_GET['aksi'];
 if($aksi=="tambah"){
     $db->input($_POST['username'],$_POST['prodi'],$_POST['email']);
     header("location:../view/tampil.php");
-}elseif($aksi=="hapus"){
+} //For add data
+elseif($aksi=="hapus"){
     $db->hapus ($_GET['id']);
     header("location:../view/tampil.php");
-}elseif($aksi=="update"){
+} //For delete data
+elseif($aksi=="update"){
     $db->update($_POST['id'],$_POST['username'],$_POST['prodi'],$_POST['emailS']);
     header("location:../view/tampil.php");
-}
+} //For update data
 ?>
 
